0829aeb697ffbc3e84bb9d692953fea8962b3a30
Guillaume Duveau <guillaume.duveau@gmail.com>
2018-02-19 14:56:14 +0100

    react-web3

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/fonts/akkmono.woff';
 import './index.css';
+import { Web3Provider } from 'react-web3';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Web3Provider>
+    <App />
+  </Web3Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
