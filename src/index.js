717ce888fa0d9aab58001192858b3b43be8afe08
Guillaume Duveau <guillaume.duveau@gmail.com>
2018-03-04 12:30:37 +0100

    workaround for font

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'purecss';
-import './assets/fonts/akkmono.woff';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+if (process.env.REACT_APP_FONT === '1') {
+  alert();
+  require('./assets/fonts/akkmono.eot');
+  require('./assets/fonts/akkmono.woff');
+  require('./fonts.css');
+}
 
 ReactDOM.render(
   <App />,
