332ab515025a740c9e45a5431913dc2c37826b7f
Guillaume Duveau <guillaume.duveau@gmail.com>
2018-03-04 12:31:32 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 if (process.env.REACT_APP_FONT === '1') {
-  alert();
   require('./assets/fonts/akkmono.eot');
   require('./assets/fonts/akkmono.woff');
   require('./fonts.css');
