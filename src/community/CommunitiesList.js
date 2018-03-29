952c1e71567b854e797797b3931a1a17ba2a7aef
Guillaume Duveau <guillaume.duveau@gmail.com>
2018-03-29 21:02:17 +0200

    grid

--- src/community/CommunitiesList.js
+++ src/community/CommunitiesList.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
-import './Communities.css';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 class CommunitiesList extends Component {
   renderItem(community, index) {
     return (
-      <p key = { index }>
-        { community.name + ' ' + community.address }
-      </p>
+      <Col key = { index } xs = { 6 } md = { 3 }>
+        <a href = { '/communities/' + community.address }>
+          { community.name }
+        </a>
+      </Col>
     )
   }
   render() {
     if (this.props.communities.length > 0) {
       return (
-        this.props.communities.map(
-          (community, index) => (
-            this.renderItem(community, index)
-          )
-        )
+        <div className = "Communities-list box blue">
+          <Grid fluid>
+            <Row>
+              {
+                this.props.communities.map(
+                  (community, index) => (
+                    this.renderItem(community, index)
+                  )
+                )
+              }
+            </Row>
+          </Grid>
+        </div>
       );
     }
     else {
